be9e2658a1a07749b72e493b8d607ff21a9fe9a8
John Gray <johngray1965@gmail.com>
2026-03-14 06:39:32 -0400

    Updated the Copyright

--- GeoKt/build.gradle.kts
+++ GeoKt/build.gradle.kts
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2026 Legere Technologies LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Some of the mathematical logic in this file is a direct translation
+ * of the C++ implementation in Google's Skia Graphics Library, which
+ * is also licensed under a permissive open-source license.
+ *
+ */
+
 import com.android.build.api.dsl.ManagedVirtualDevice
 import org.gradle.kotlin.dsl.create
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
@@ -87,6 +108,7 @@ kotlin {
             implementation(libs.kotlin.test)
             implementation(libs.assertk)
         }
+        @Suppress("unused")
         val androidHostTest by getting {
             dependencies {
                 implementation(libs.kotlin.test)
@@ -95,6 +117,7 @@ kotlin {
                 implementation(libs.junit)
             }
         }
+        @Suppress("unused")
         val androidDeviceTest by getting {
             dependencies {
                 implementation(libs.kotlin.test)
